f32f312aa2e2cbe019f2afa7f008e499ff335642
kubrub <keshka3455@gmail.com>
2022-03-03 19:53:57 +0200

    Change location of file

--- src/main/java/CodeWars/SevenKata/LineNumbering.java
+++ src/main/java/CodeWars/SevenKata/LineNumbering.java
@@ -1,4 +1,4 @@
-package CodeWars.EightKata;
+package CodeWars.SevenKata;
 /*Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
 
 Write a function which takes a list of strings and returns each line prepended by the correct number.
